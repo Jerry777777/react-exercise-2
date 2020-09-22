79922bd4ad78ffbd5a82976e180e733d566e49e9
Jerry777777 <491818175@qq.com>
2020-09-22 23:21:39 +0800

    [xiawenhao] feat:add header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <main className="app">
+        <Header />
       </main>
     );
   }
 }
-
 export default App;
